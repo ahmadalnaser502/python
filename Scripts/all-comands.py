466739c26502efa246d1bbbc3aba56f63a03d4c8
Ahmad Naser <Ahmad.Naser@Realsoft-me.com>
2025-08-05 07:17:48 +0300

    variable lesson

---
+++ Scripts/all-comands.py
@@ -0,0 +1,6 @@
+# 1- print ("")
+print ("Hello World ")
+
+# 2- type (variable)
+age = 5 
+print (type (age))
---
+++ Scripts/script-05082025.py
@@ -0,0 +1,14 @@
+# first commands 
+print("this is my first command ")
+
+# variable 
+first_name = "Ahmad"
+last_name = "Naser"
+postion = "DevOps Engineer"
+
+age = "26"
+salary = "5000"
+
+married = True
+over5years= False
+
